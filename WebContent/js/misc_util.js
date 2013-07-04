7281ab035494a7914e0d402bfeeee19a3bd28015
gene-sbay <geneb.contact@gmail.com>
2013-07-04 15:57:52 -0700

    cleaned up js util code

--- WebContent/js/misc_util.js
+++ WebContent/js/misc_util.js
@@ -1,40 +1,7 @@
 
 
 // =====================
-// AJAX
-
-var AJAX_REQUEST;
-
-function doAjaxRequest(url, showResultFunction) 
-{
-	if (window.ActiveXObject) 
-	{
-		AJAX_REQUEST = new ActiveXObject("Microsoft.XMLHTTP");
-	} 
-	else if (window.XMLHttpRequest) 
-	{
-		AJAX_REQUEST = new XMLHttpRequest();
-	}
-	
-	AJAX_REQUEST.onreadystatechange = showResultFunction;
-	AJAX_REQUEST.open("POST", url, true);
-	AJAX_REQUEST.send();
-}
-
-function getAjaxResponse()
-{
-		// has to == 4 in order for SUCCESS - all other values, we do nothing
-	if (AJAX_REQUEST.readyState != 4) 
-		return null;
-
-	var response = AJAX_REQUEST.responseXML;
-
-	return response;
-}
-
-
-// =====================
-// MISC
+// STRING UTILS
 
 var MAX_DUMP_DEPTH = 10;
 
