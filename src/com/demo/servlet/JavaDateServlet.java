0240228adee90b71056160961a6dd47ce14a4556
gene-sbay <geneb.contact@gmail.com>
2013-07-05 14:44:50 -0700

    added auto ajax_pagerefresh demo

--- src/com/demo/servlet/JavaDateServlet.java
+++ src/com/demo/servlet/JavaDateServlet.java
@@ -2,6 +2,7 @@ package com.demo.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -29,15 +30,22 @@ public class JavaDateServlet extends HttpServlet
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request,
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
 	{
 		response.setContentType("text/xml");
 		String param1 = request.getParameter("param1"); // pending/optional
 		
-		long curDateString = (new Date()).getTime();
+		Date date = new Date();
+		
+		long curDateString = (date).getTime();
+		
+		SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        String dateText = df2.format(date);
+        
 		String responseXml = "<JavaDate>";
 		responseXml += "<long>" + curDateString + "</long>";
+		responseXml += "<text>" + dateText + "</text>";
 		responseXml += "</JavaDate>";
 	
 		System.out.println("** SENDING xml:  = " + responseXml);
@@ -49,7 +57,7 @@ public class JavaDateServlet extends HttpServlet
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
+	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
 	{
 		PrintWriter out = response.getWriter();
